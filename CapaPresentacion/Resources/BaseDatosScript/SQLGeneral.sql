6bf24d27c15fcd38b1cb55883e1cd6ae29f6f628
YERMIN YAIR LINO SANCHEZ <yermin.linos@ug.edu.ec>
2022-01-10 20:42:36 -0500

    cambio

--- CapaPresentacion/Resources/BaseDatosScript/SQLGeneral.sql
+++ CapaPresentacion/Resources/BaseDatosScript/SQLGeneral.sql
@@ -75,13 +75,11 @@ CREATE TABLE Ambulancia
   Id_Ambulancia      int  identity (1,1) not null,
   modelo         VARCHAR (40) not null,
   tipoAmbulancia              VARCHAR (40) not null ,
-  placa           VARCHAR (40) unique not null ,
-  matricula                 VARCHAR(40)  not null ,
-  fechaActivacion            VARCHAR(40) not null,
-  estado	               VARCHAR(40) not null,
-  observacion 		   VARCHAR(40) not null,
+  placa           VARCHAR (40) unique not null,
+  matricula                 VARCHAR(40) unique not null,
   Primary Key(Id_Ambulancia)
 );
+-- drop table Ambulancia;
 
 
 /* ---------------------- CONDUCTOR ------------------------ */
