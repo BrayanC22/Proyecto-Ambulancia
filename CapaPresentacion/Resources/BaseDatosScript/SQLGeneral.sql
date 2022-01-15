ba07f713ac4effc82eb8a85177ecb1cc163dd933
YERMIN YAIR LINO SANCHEZ <yermin.linos@ug.edu.ec>
2022-01-14 20:10:25 -0500

    Correccion del script (O eso creo)

--- CapaPresentacion/Resources/BaseDatosScript/SQLGeneral.sql
+++ CapaPresentacion/Resources/BaseDatosScript/SQLGeneral.sql
@@ -315,6 +315,23 @@ AS
 UPDATE [Ambulancia] SET [modelo] = @modelo, [tipoAmbulancia] = @tipoAmbulancia, [matricula] = @matricula WHERE ([placa] = @placa);
 
 GO
+
+/* ---------------------- CONDUCTOR ------------------------ */
+
+CREATE TABLE Conductor
+(
+  Id_Conductor      int  identity (1,1) not null,
+  Cedula         VARCHAR (60) unique not null,
+  Nombre             VARCHAR (60) not null ,
+  Apellido           VARCHAR (60) not null ,
+  Edad                 int  not null ,
+  Domicilio           VARCHAR(60) not null,
+  Sexo	               VARCHAR(60) not null,
+  Licencia             VARCHAR(60) not null,
+
+  Primary Key(Id_Conductor)
+);
+
 /* ---------------------- Asignacion ------------------------ */
 
 CREATE TABLE Asignacion
@@ -357,21 +374,7 @@ INSERT INTO [Asignacion] ([Id_AmbulanciaAsignacion], [Id_ConductorAsignacion]) V
 SELECT Id_Asignacion, Id_AmbulanciaAsignacion, Id_ConductorAsignacion FROM Asignacion WHERE (Id_Asignacion = SCOPE_IDENTITY())
 GO
 
-/* ---------------------- CONDUCTOR ------------------------ */
 
-CREATE TABLE Conductor
-(
-  Id_Conductor      int  identity (1,1) not null,
-  Cedula         VARCHAR (60) unique not null,
-  Nombre             VARCHAR (60) not null ,
-  Apellido           VARCHAR (60) not null ,
-  Edad                 int  not null ,
-  Domicilio           VARCHAR(60) not null,
-  Sexo	               VARCHAR(60) not null,
-  Licencia             VARCHAR(60) not null,
-
-  Primary Key(Id_Conductor)
-);
 
 
 
